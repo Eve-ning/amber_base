dec591f50fe0719ea8975afb8f296a704c59ef3a
Eve-ning <johnchangqi@hotmail.com>
2018-12-25 21:56:02 +0800

    Added ppdefinition

--- custom_functions/split_string.cpp
+++ custom_functions/split_string.cpp
@@ -1,1 +1,17 @@
 #include "split_string.h"
+
+std::vector<std::string> split_string::by_delimeter(std::string str, char delimeter, bool end_delimeter) {
+	std::vector<std::string> output = {};
+
+	str.push_back(delimeter); // Make sure the while loop includes the last token
+	std::string temp_ = "";
+	while (str.find(delimeter) != std::string::npos) {
+
+		temp_ = str.substr(0, str.find(delimeter)); // Holds the token temporarily
+
+		output.push_back(temp_); // Push to output
+		str.erase(0, temp_.length() + 1); // Remove token and delimeter length
+	}
+
+	return output;
+}
--- custom_functions/split_string.h
+++ custom_functions/split_string.h
@@ -1,21 +1,13 @@
 #pragma once
 #include <vector>
 #include <string>
+
+#ifdef AMBER_BASE_EX                                                // Declare this when compiling the library!
+#define AMBER_BASE __declspec(dllexport)                       
+#else
+#define AMBER_BASE __declspec(dllimport)
+#endif
 namespace split_string {
-	std::vector<std::string> by_delimeter(std::string str, char delimeter = '\n', bool end_delimeter = false) {
-		std::vector<std::string> output = {}; 
-
-		str.push_back(delimeter); // Make sure the while loop includes the last token
-		std::string temp_ = "";
-		while (str.find(delimeter) != std::string::npos) {
-
-			temp_ = str.substr(0, str.find(delimeter)); // Holds the token temporarily
-
-			output.push_back(temp_); // Push to output
-			str.erase(0, temp_.length() + 1); // Remove token and delimeter length
-		}
-
-		return output;
-	}
+	std::vector<std::string> by_delimeter(std::string str, char delimeter = '\n', bool end_delimeter = false);
 }
 
