812bf000e1a55da49c4e45b116067cb511d9f73d
Eve-ning <johnchangqi@hotmail.com>
2019-01-06 21:39:26 +0800

    Reorganize functions

--- src/objects/multiple/osu_object_v.h
+++ src/objects/multiple/osu_object_v.h
@@ -81,39 +81,6 @@ public:
 		return output;
 	}
 
-	// Appends to back of vector
-	void push_back(const obj_type& obj) {
-		m_object_v.push_back(obj);
-	}
-	// Appends vector to back of vector
-	void push_back(osu_object_v obj_v) {
-		for (const obj_type& obj : obj_v) {
-			push_back(obj);
-		}
-	}
-
-	osu_object_v& operator =(std::vector<std::shared_ptr<osu_object>> obj_sptr_v) {
-		load_obj_sptr(obj_sptr_v);
-		return *this;
-	}
-	bool operator ==(const osu_object_v &obj_v) const {
-		bool flag = true;
-		size_t input_size = obj_v.size();
-
-		if (obj_v.size() != input_size) {
-			return false; // Mismatch in size
-		}
-
-		size_t x = 0;
-		while (x < input_size && (flag == true)) {
-			// If any mismatch, flag will be false;
-			flag &= (obj_v[0] == m_object_v[0]);
-			x++;
-		}
-
-		return flag;
-	}
-
 	std::vector<double> get_offset_v() const {
 		std::vector<double> offset_v = {};
 		std::transform(begin(), end(), std::back_inserter(offset_v), [](const obj_type &obj) {
@@ -121,25 +88,6 @@ public:
 		});
 		return offset_v;
 	}
-
-	obj_type get_index(unsigned index) const {
-		return m_object_v[index];
-	}
-	obj_type & get_index(unsigned index) {
-		return m_object_v[index];
-	}
-
-	obj_type operator [](unsigned int i) const { return get_index(i); }
-	obj_type & operator [](unsigned int i) { return get_index(i); }
-
-	// Direct all iterator functions to the vector
-	typename std::vector<obj_type>::iterator begin() { return m_object_v.begin(); }
-	typename std::vector<obj_type>::iterator end() { return m_object_v.end(); }
-	typename std::vector<obj_type>::const_iterator begin() const { return m_object_v.cbegin(); }
-	typename std::vector<obj_type>::const_iterator end() const { return m_object_v.cend(); }
-
-	obj_type front() const { return m_object_v.front(); }
-	obj_type back() const { return m_object_v.back(); }
 
 	void sort_by_offset(bool ascending = true) {
 		std::sort(begin(), end(), [=](obj_type& obj1, obj_type& obj2) {
@@ -194,6 +142,57 @@ public:
 		return get_last_object_by_offset().get_offset();
 	}
 
+	// Appends to back of vector
+	void push_back(const obj_type& obj) {
+		m_object_v.push_back(obj);
+	}
+	// Appends vector to back of vector
+	void push_back(osu_object_v obj_v) {
+		for (const obj_type& obj : obj_v) {
+			push_back(obj);
+		}
+	}
+
+	osu_object_v& operator =(std::vector<std::shared_ptr<osu_object>> obj_sptr_v) {
+		load_obj_sptr(obj_sptr_v);
+		return *this;
+	}
+	bool operator ==(const osu_object_v &obj_v) const {
+		bool flag = true;
+		size_t input_size = obj_v.size();
+
+		if (obj_v.size() != input_size) {
+			return false; // Mismatch in size
+		}
+
+		size_t x = 0;
+		while (x < input_size && (flag == true)) {
+			// If any mismatch, flag will be false;
+			flag &= (obj_v[0] == m_object_v[0]);
+			x++;
+		}
+
+		return flag;
+	}
+
+	obj_type get_index(unsigned index) const {
+		return m_object_v[index];
+	}
+	obj_type & get_index(unsigned index) {
+		return m_object_v[index];
+	}
+
+	obj_type operator [](unsigned int i) const { return get_index(i); }
+	obj_type & operator [](unsigned int i) { return get_index(i); }
+
+	// Direct all iterator functions to the vector
+	typename std::vector<obj_type>::iterator begin() { return m_object_v.begin(); }
+	typename std::vector<obj_type>::iterator end() { return m_object_v.end(); }
+	typename std::vector<obj_type>::const_iterator begin() const { return m_object_v.cbegin(); }
+	typename std::vector<obj_type>::const_iterator end() const { return m_object_v.cend(); }
+
+	obj_type front() const { return m_object_v.front(); }
+	obj_type back() const { return m_object_v.back(); }
 
 protected:
 	std::vector<obj_type> m_object_v;
