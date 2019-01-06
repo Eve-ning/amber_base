99a743379314d795bfbf50c2b40509afc5c8de79
Eve-ning <johnchangqi@hotmail.com>
2019-01-06 18:09:40 +0800

    Remove outdated definition in lib cpp

--- src/custom_lib_functions/lib_functions.cpp
+++ src/custom_lib_functions/lib_functions.cpp
@@ -1,15 +1,4 @@
 #include "lib_functions.h"
 
 
-std::vector<std::shared_ptr<osu_object>> lib_functions::create_copies(const std::shared_ptr<osu_object>& obj, std::vector<double> copy_to_v) {
-
-	std::vector<std::shared_ptr<osu_object>> output = {};
-	// For each offset to copy to
-	for (double copy_to : copy_to_v) {
-		std::shared_ptr<osu_object> temp_ = obj->clone();
-		temp_->set_offset(copy_to);
-		output.push_back(temp_);
-	}
-	return output;
-}
 
