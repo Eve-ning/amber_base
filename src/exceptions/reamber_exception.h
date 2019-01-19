bd59ebb542390cd9a8a081ac285aa9555868f67d
Eve-ning <johnchangqi@hotmail.com>
2019-01-19 18:37:07 +0800

    Fixed reamber_exception not included in library

--- src/exceptions/reamber_exception.h
+++ src/exceptions/reamber_exception.h
@@ -3,9 +3,15 @@
 #include <exception>
 #include <string>
 
+#ifdef AMBER_BASE_EX                                                // Declare this when compiling the library!
+#define AMBER_BASE __declspec(dllexport)                       
+#else
+#define AMBER_BASE __declspec(dllimport)
+#endif
+
 // Handles all of reamber's exceptions
 
-class reamber_exception : public std::exception
+class AMBER_BASE reamber_exception : public std::exception
 {
 public:
     reamber_exception(const char* msg);
