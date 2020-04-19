5399b61b75563d7c7df088e834866632b1cd112b
Evening <johnchangqi@hotmail.com>
2020-04-19 16:34:27 +0800

    Fix extra lines

--- src/object/singular/osuobject.cpp
+++ src/object/singular/osuobject.cpp
@@ -1,37 +1,30 @@
 #include "object/singular/osuobject.h"
 #include <QtGlobal>
 
-OsuObject::OsuObject() : offset(-1){}
-
+OsuObject::OsuObject() : offset(-1) {}
 OsuObject::OsuObject(double new_offset) : offset(new_offset)
 {
 
 }
-
 OsuObject::OsuObject(const OsuObject &obj) {
     offset = obj.offset;
 }
-
 OsuObject &OsuObject::operator=(const OsuObject &o){
     if (this == &o) return *this;
     this->offset = o.offset;
     return *this;
 }
 
-double OsuObject::getOffset() const
-{
+double OsuObject::getOffset() const{
     return offset;
 }
-
-void OsuObject::setOffset(double value)
-{
+void OsuObject::setOffset(double value) {
     offset = value;
 }
 
 bool OsuObject::operator <(const OsuObject &ho) const {
     return offset < ho.offset;
 }
-
 bool OsuObject::operator ==(const OsuObject &ho) const {
     return qFuzzyCompare(offset, ho.offset);
 }
